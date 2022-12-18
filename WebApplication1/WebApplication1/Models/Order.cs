aeb50dc15f672fed485d9fcbb839de1c25e2aed2
zeroking99 <101686510+zeroking99@users.noreply.github.com>
2022-12-18 17:13:43 +0700

    â

--- WebApplication1/WebApplication1/Models/Order.cs
+++ WebApplication1/WebApplication1/Models/Order.cs
@@ -6,17 +6,25 @@ namespace WebApplication1.Models
 {
     public class Order
     {
-        
         public int Id { get; set; }
-        public int Email { get; set; }
+         
         public int BookId { get; set; }
-        public Payment Payment { get; set; }
-        public DateTime Order_Date { get; set; }
-        [Required]
-        public int Quantity { get; set; }
-        public int Customer { get; set; }
-        public Customer Customers { get; set; }
+
+        public string Title { get; set; }
+
+        //Order - Mobile: Many To One
         public Book Book { get; set; }
+        public string UserEmail { get; set; }
+
+        [Required]
+        public int OrderQuantity { get; set; }
+
+        [Required]
+        public double OrderPrice { get; set; }
+
+        [Required]
+        [DataType(DataType.DateTime)]
+        public DateTime OrderDate { get; set; }
     }
     public enum Payment
     {
